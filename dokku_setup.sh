c126ee7d1177fe669c435a0bdcf747c2f862340c
Andre Alves Garzia <andre@andregarzia.com>
2017-11-21 21:57:28 +0000

    dokku problems

--- dokku_setup.sh
+++ dokku_setup.sh
@@ -1,7 +1,7 @@
-sudo apt-get install build-essential -y
-curl -o /tmp/rustup-init.sh "https://sh.rustup.rs"
-wait
-sh /tmp/rustup-init.sh -y
-export PATH="$HOME/.cargo/bin:$PATH"
+# sudo apt-get install build-essential -y
+#curl -o /tmp/rustup-init.sh "https://sh.rustup.rs"
+#wait
+#sh /tmp/rustup-init.sh -y
+#export PATH="$HOME/.cargo/bin:$PATH"
 npm i -g neon-cli
 neon build
