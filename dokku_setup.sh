53af43c094e44ddded60fcd9eb441eee1f47018b
Andre Alves Garzia <andre@andregarzia.com>
2017-11-21 21:11:31 +0000

    dokku problems

--- dokku_setup.sh
+++ dokku_setup.sh
@@ -1,4 +1,6 @@
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl https://sh.rustup.rs -o /tmp/rustup-init.sh
+wait
+sh /tmp/rustup-init.sh -y
 export PATH="$HOME/.cargo/bin:$PATH"
 npm i -g neon-cli
 neon build
