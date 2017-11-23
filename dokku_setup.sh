99c5675b72d95ef2ac0f6619c0245a68fe26dac2
andre alves garzia <andre@andregarzia.com>
2017-11-23 20:34:45 -0200

    poc ready for distribution

--- dokku_setup.sh
+++
@@ -1,7 +0,0 @@
-# sudo apt-get install build-essential -y
-#curl -o /tmp/rustup-init.sh "https://sh.rustup.rs"
-#wait
-#sh /tmp/rustup-init.sh -y
-#export PATH="$HOME/.cargo/bin:$PATH"
-npm i -g neon-cli
-neon build
